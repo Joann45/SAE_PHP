ef16fa96bbe93d647c4107045139d6e9af5e88f9
Fournier Yael <o22300465@or-iut-i205-f02.iut45.univ-orleans.fr>
2025-02-03 16:59:14 +0100

    ajout du routeur et de quelques routes fictives

--- Controlleurs/Routeur.php
+++ Controlleurs/Routeur.php
@@ -3,7 +3,24 @@
 namespace Controlleurs;
 
 class Routeur{
+    public function handleRequest(){
+        $data = $SERVER['REQUEST_URI'];
 
+        switch($data){
+            case '/':
+                require_once VIEWS_PATH . '/accueil.php';
+                break;
+            case '/connexion':
+                require_once VIEWS_PATH . '/connexion.php';
+                break;
+            case '/avis_user':
+                require_once VIEWS_PATH . '/avis_user.php';
+                break;
+            default:
+                require_once VIEWS_PATH . '/404.php';
+                break;
+        }
+    }
 }
 
 ?>
