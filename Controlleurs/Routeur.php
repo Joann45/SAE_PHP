c08337aa3c14f3a1be9928b69cd3ffbefaf6968b
Fournier Yael <o22300465@or-iut-i205-f02.iut45.univ-orleans.fr>
2025-02-03 16:49:11 +0100

    Revert "mise a jour de l'arboresence"
    This reverts commit b90d82288ad98a22026df3039fa848b166082d42.

--- Controlleurs/Routeur.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Controlleurs;
-
-class Routeur{
-
-}
-
-?>
--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <?php
 
-require_once __DIR__ . '/Controlleurs/Routeur.php';
-require_once __DIR__ . '/Controlleurs/config.php';
 require_once __DIR__ . '/BD/SupabaseLoader.php';
 require_once __DIR__ . '/AutoLoader.php';
 
 use BD\SupabaseLoader;
-use Controlleurs\Routeur;
 
 AutoLoader::register();
 
 $connection = SupabaseLoader::getConnection();
 
-//SupabaseLoader::query($connection, "INSERT INTO utilisateur VALUES (1, 'test', 'test', 'test', 'test', 'test')");
-
-$routeur = new Routeur();
-$routeur->handleRequest();
+SupabaseLoader::query($connection, "INSERT INTO utilisateur VALUES (1, 'test', 'test', 'test', 'test', 'test')");
 
 ?>
