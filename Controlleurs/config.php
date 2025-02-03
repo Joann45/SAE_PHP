d3b05912df7462dd275f4efa0ce2be9f278f10bf
Fournier Yael <o22300465@or-iut-i205-f02.iut45.univ-orleans.fr>
2025-02-03 16:55:35 +0100

    fix arborecense

---
+++ Controlleurs/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('BASE_PATH', __DIR__ . '/..');
+define('SRC_PATH', BASE_PATH . '/src');
+define('BD_PATH', BASE_PATH . '/BD');
+define('CONTROLLERS_PATH', BASE_PATH . '/Controlleurs');
+define('VIEWS_PATH', SRC_PATH . '/Views');
+define('MODELS_PATH', SRC_PATH . '/Modèle');
+
+define('CURRENT_PAGE', basename($_SERVER['PHP_SELF'], '.php'));
+?>
